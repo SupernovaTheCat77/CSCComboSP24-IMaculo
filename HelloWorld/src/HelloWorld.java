9051b9d946adfe8077c4731f89b2cc161ed6a936
Maculo <S02590993@ccc.ccofc.edu>
2024-02-01 10:07:44 -0700

    Hello world, first commit :)

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("Hello" + " " + "World");
+		
+		System.out.print("Hello");
+		System.out.print(" ");
+		System.out.println("World");
+		
+		System.out.println("H" + "e" + "l" + "l" + "o" + " " + "W" + "o" + "r" + "l" + "d");
+	}
+
+}
